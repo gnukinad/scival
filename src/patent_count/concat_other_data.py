0706167365ecf58fe6bb354bc44f110ccfd188b4
DaniyarBakir <daniyar.bakir@nu.edu.kz>
2018-04-02 10:36:35 +0600

    moved book and patent count codes to separate folders

---
+++ src/patent_count/concat_other_data.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+import os
+import glob
+import re
+
+import logging
+from pprint import pprint as pp
+import json
+
+
+def get_patent_count(fname_book, fname_res):
+
+    # fname_book = '../data/patent_count.csv'
+    # fname_res = '../data/long_patent_count.csv'
+    df_book = pd.read_csv(fname_book)
+
+    patent_cols = ['patent_count_2012', 'patent_count_2013', 'patent_count_2014', 'patent_count_2015', 'patent_count_2016']
+    convert_dict = {'patent_count_2012': 2012,
+                    'patent_count_2013': 2013,
+                    'patent_count_2014': 2014,
+                    'patent_count_2015': 2015,
+                    'patent_count_2016': 2016}
+
+    years = [2012, 2013, 2014, 2015, 2016]
+    pname = 'patent_count'
+
+    df = pd.DataFrame(columns=['name', 'year', 'metricType', 'valueByYear'])
+
+    se = re.compile(pname)
+
+    for index, row in df_book.iterrows():
+
+        for sindex, svalue in row.iteritems():
+
+            if se.search(sindex):
+
+                if not np.isnan(svalue):
+
+                    df.at[index, 'name'] = row['name']
+                    df.at[index, 'metricType'] = 'PatentCount'
+                    splitted = sindex.split('_')
+                    year = splitted[-1]
+                    df.at[index, 'valueByYear'] = svalue
+                    df.at[index, 'year'] = year
+
+    df.reset_index().to_csv(fname_res, index=False)
+
+
+def get_book_count(res):
+
+    nbooks = res['opensearch:totalResults']
+    search_query = res['opensearch:Query']['@searchTerms']
+
+    splitted = search_query.split('AND')
+
+    se = re.compile('affil', re.IGNORECASE)
+
+    q = [x for x in splitted if se.search(x)][0].strip()
+    aff_name = q[q.find("(")+1:q.find(")")].strip('"')
+
+    return {'name': aff_name,
+            'book_count': nbooks}
+
+
+def append_book_count(res_name, year):
+
+    # res_name = '../data/book_count.csv'
+
+    df = pd.DataFrame()
+
+    res = dict()
+
+    # year = 2014
+
+    dname_book_count = '../data/aff_book_' + str(year)
+    fnames = os.listdir(dname_book_count)
+
+    for x in fnames:
+        # print('reading ', x)
+        with open('../data/aff_book_' + str(year) + '/' + x) as f:
+            txt = f.readlines()
+
+        jres = json.loads(txt[0])
+
+        a = get_book_count(jres['search-results'])
+
+        q = {'name': a['name'],
+             'metricType': 'BookCount',
+             'year': year,
+             'valueByYear': a['book_count']}
+
+        df = df.append(q, ignore_index=True)
+
+    try:
+        df_old = pd.read_csv(res_name)
+    except:
+        df_old = pd.DataFrame(columns=['name', 'metricType', 'year', 'valueByYear'])
+
+    df = pd.concat([df, df_old])
+
+    df.to_csv(res_name, index=False)
+    return df
+
+
+if __name__ == "__main__":
+
+    fname_long_book = '../data/long_book_count.csv'
+    fname_long_patent  = '../data/long_patent_count.csv'
+
+    years = [2012, 2013, 2014, 2015, 2016]
+    # years = [2012]
+    for x in years:
+        df = append_book_count(fname_long_book, x)
+
+
+    fname_book = '../data/patent_count.csv'
+    # get_patent_count(fname_book, fname_long_patent)
+
+    '''
+    dname_metric = os.path.join("..", "data", "metric_response")
+
+    drop_cols = ['uri', 'type', 'link']
+    excel_fname = "long_table.xlsx" # soon be renamed into all_metrics.xlsx
+
+    all_fnames = glob.glob(os.path.join(dname_metric, "*.csv"))
+
+    df_list = [pd.DataFrame(pd.read_csv(x)) for x in all_fnames]
+    df_metric = pd.concat(df_list)
+
+
+    df_patent = pd.read_csv(fname_long_book)
+    df_book   = pd.read_csv(fname_long_patent)
+
+    df = pd.concat([df_metric, df_patent, df_book], ignore_index=True)
+
+    df.to_csv('../data/long_all_metrics.csv', index=False)
+    '''
---
+++ src/patent_count/get_patent_count.py
@@ -0,0 +1,357 @@
+import urllib.parse as urlp
+import os
+import numpy as np
+import pandas as pd
+from pprint import pprint as pp
+import logging
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import time
+from numpy.random import uniform as runi
+
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+BASE_DIR = os.path.abspath(os.path.realpath(__file__))
+BASE_DIR = os.path.join(os.path.dirname(BASE_DIR), '..', '..')
+os.chdir(BASE_DIR)
+
+FOLNAME_AFF_SEARCH = os.path.join(BASE_DIR, 'data', 'aff_search')
+FOLNAME_METRIC_RESPONSE = os.path.join(BASE_DIR, 'data', 'metric_response')
+
+key_aff = 'Institution'
+key_acc = 'id_downloaded'
+key_id  = 'id'
+key_met = 'metrics'
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create file handler which logs info messages
+fh = logging.FileHandler(os.path.join(BASE_DIR, 'logs', 'logs.txt'), 'w', 'utf-8')
+fh.setLevel(logging.DEBUG)
+
+# create console handler with a debug log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# creating a formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)-8s: %(message)s')
+
+# setting handler format
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+def get_valid_ids(df, key, n, valid_values=None, bad_values=None):
+    '''this function gets the valid indices'''
+
+    valid_values = [np.nan] or valid_values
+    bad_values = [-1] or bad_values
+
+    # a = df.loc[:, key]
+
+    valid_inds = []
+
+    inds = df.index.tolist()
+    print(inds[:10])
+
+    i = 0
+    j = 0
+
+    while(True):
+
+        if np.isnan(df.loc[inds[j], key]):
+            valid_inds.append(inds[j])
+            i = i+1
+
+        if i == n:
+            break
+
+        if j > df.shape[0]:
+            break
+
+        if i % 10 == 0 and i != 0:
+            logger.debug('getting valid inds, i is {}'.format(i))
+
+        j = j + 1
+
+    return valid_inds
+
+
+
+
+class my_df(pd.DataFrame):
+
+    def __init__(self):
+
+        super().__init__(self)
+        self.i = 0
+
+    def my_iterrows(self):
+        a = self.iterrows()
+        self.i = self.i + 1
+        return a
+
+    def get_valid_id(self, key=None):
+
+        q = 0
+
+        while(True):
+            
+            a = next(self.iterrows())
+
+            print(a)
+            q = q + 1
+
+            if q % 5 == 0:
+                break
+            
+
+def open_scopus_link(driver):
+    '''
+    open the advanced search of scopus
+    '''
+    # binary = FirefoxBinary('/usr/lib/firefox/firefox')
+    # driver = webdriver.Firefox(firefox_binary=binary)
+    driver.implicitly_wait(2) # seconds
+
+    # l = 'https://www.nobelprize.org/nobel_prizes/physics/laureates/index.html'
+    main_scopus2 = 'https://www.scopus.com/sources?zone=&origin=NO%20ORIGIN%20DEFINED'
+    main_search = 'https://www.scopus.com/search/form.uri?zone=TopNavBar&origin=sbrowse&display=basic'
+    adv_search = 'https://www.scopus.com/search/form.uri?display=advanced&clear=t&origin=searchbasic&txGid=fc476bc6f6c3a112a577edd9f6f26e14'
+
+    # to get to advanced search, we need to go through several links
+    driver.get(main_scopus2)
+    t = runi(10 + runi(-3, 1))
+    logger.debug('opened main link, waiting for {} s'.format(t))
+    time.sleep(t)
+    # driver.implicitly_wait(10) # seconds
+
+    driver.get(main_search)
+    t = runi(10 + runi(-3, 1))
+    logger.debug('opened search link, waiting for {} s'.format(t))
+    time.sleep(t)
+    # driver.implicitly_wait(10) # seconds
+
+    driver.get(adv_search)
+    t = runi(10 + runi(-3, 1))
+    logger.debug('opened advanced search link, waiting for {} s'.format(t))
+    time.sleep(t)
+    # driver.implicitly_wait(10) # seconds
+
+    return driver
+
+
+def extract_integer(a):
+    n = [np.int(x) for x in a.split() if x.isdigit()] or 0
+    return n[0]
+
+
+def unparse(l):
+    main_link, query = l.split('?')
+
+    a = query.split('&')
+
+    b = [x.split('=') for x in a]
+    params = dict(zip([x[0] for x in b], [x[1] for x in b]))
+
+    q = params['s'].split('AND')
+    qparams = [urlp.unquote_plus(x).strip() for x in q]
+
+    params['q'] = qparams
+    params['main_link'] = main_link
+
+    return params
+
+
+def get_patent_count(driver, query):
+
+    # click to activate the textField
+    logger.debug('clicking on contentEditLabel')
+    try:
+        driver.find_element_by_id('contentEditLabel').click()
+    except Exception as e:
+        driver.find_element_by_id('searchfield').click()
+
+
+
+    # fill the textfield and send the request
+    element = driver.find_element_by_id('searchfield')
+
+    logger.debug('clearing search field')
+    element.clear()
+    # time.wait(runi(0, 0.2))
+    logger.debug('entering query into search field')
+    element.send_keys(query, Keys.RETURN)
+
+
+    logger.debug('waiting for page to be downloaded')
+    # get amount of patents
+    el = wait(driver, 5).until(EC.presence_of_element_located((By.ID, "searchResFormId")))
+
+    current_url = driver.current_url
+    driver.get(current_url)
+
+    t = 4 + runi(0, 1)
+    time.sleep(t)
+
+    try:
+        logger.debug('retrieving patent.count')
+        patent_hidden_element = driver.find_element_by_id('hubLinksContainer')
+        if patent_hidden_element.get_attribute('class') == 'hidden':
+            patent_value = 0
+        else:
+            patent_element = driver.find_element_by_id('patentLink')
+            patent_value = patent_element.text
+    
+            try:
+                a  = extract_integer(patent_value)
+            except:
+                a = 0
+    
+        t = 2 + runi(0, 1)
+        time.sleep(t)
+    
+        driver.find_element_by_id('editAuthSearch').click()
+    
+        t = 2 + runi(0, 1)
+        time.sleep(t)
+    
+        return a
+    except:
+        driver.find_element_by_xpath("//button[@title='Edit search query']").click()
+        assert 'something happened'
+
+
+'''
+if __name__=="__main__":
+
+    n = 3
+
+    year = 2013
+
+    fname_df = os.path.join('data', 'universities_table.csv')
+    fname_res = os.path.join('data', 'patent_count.csv')
+'''
+
+
+def main(n, year, fname_df, fname_res):
+
+    slink = 'https://www.scopus.com/results/results.uri?sort=plf-f&src=s&sid=9cf773e35f9165af3488aaee575fd58a&sot=a&sdt=a&sl=49&s=affil%28%22University+of+Toronto%22%29+AND+pubyear+%3d+2016&origin=searchadvanced&editSaveSearch=&txGid=7bfd7d6ce111fb51284b6d3d7d6149ee'
+
+    plink = 'https://www.scopus.com/results/results.uri?src=p&sort=plf-f&sid=9cf773e35f9165af3488aaee575fd58a&sot=a&sdt=a&sl=49&s=affil%28%22University+of+Toronto%22%29+AND+pubyear+%3d+2016&cl=t&offset=1&ss=plf-f&ws=r-f&ps=r-f&cs=r-f&origin=resultslist&zone=queryBar'
+
+    simple = unparse(slink)
+    patent = unparse(plink)
+
+    # n = 15
+    # year = 2015
+
+    logger.debug('downloaded results table')
+    # fname_res = os.path.join('data', 'patent_count.csv')
+
+    res_ind_name = 'name'
+    res_col_name = 'patent_count_{}'.format(year)
+    res = pd.read_csv(fname_res).set_index(res_ind_name)
+    
+
+    # fname_df = os.path.join('data', 'universities_table.csv')
+    # fname_res = os.path.join('data', 'patent_count.csv')
+    logger.debug('loading df with acknowledgements')
+    df = pd.read_csv(fname_df).set_index('Institution')
+
+
+    key = 'patent_downloaded_{}'.format(year)
+    logger.debug('loading affiliations that needs to be processed, key is {}, n is {}'.format(key, n))
+    valid_aff_names = get_valid_ids(df, key, n)
+    # valid_aff_names = get_valid_ids(df.set_index('Institution'), key, n)
+
+    logger.debug('opening browser with scopus advanced search link')
+
+    timeout = 30
+
+    binary = FirefoxBinary('/usr/lib/firefox/firefox')
+    driver = webdriver.Firefox(firefox_binary=binary)
+    driver.set_page_load_timeout(timeout) # error if timeout has passed
+    
+    try:
+        driver = open_scopus_link(driver)
+
+        curd = {res_ind_name: [],
+            res_col_name: []}
+
+        logger.debug('doing search')
+    except:
+        logger.warning('error has occured during opening browser')
+    else:
+
+        # for i in range(n):
+        for x in valid_aff_names:
+            aff_name = x
+            # aff_name = valid_aff_names[i]
+            patent_count = 0
+    
+            logger.debug('creating query for search')
+            query = 'affil("{}") AND pubyear = {}'.format(aff_name, year)
+            logger.debug('query is {}'.format(query))
+    
+            try:
+                logger.debug('getting patent_count for {}'.format(aff_name))
+                patent_count = get_patent_count(driver, query)
+                
+                logger.debug('number of patents for {} is {}'.format(aff_name, patent_count))
+                logger.debug('number of patents for {} is {}'.format(aff_name, patent_count))
+                logger.debug('updating acknoledgment df at [{}, {}]'.format(aff_name, key))
+                df.at[aff_name, key] = 1
+    
+                logger.debug('updating the resultant dictionary')
+                curd[res_ind_name].append(aff_name)
+                curd[res_col_name].append(patent_count)
+    
+            except Exception as e:
+                logger.warning('error has occured')
+    
+                df.at[aff_name, key] = -1
+    
+    
+        logger.debug('creating the resultant DataFrame')
+        cur_df = pd.DataFrame(curd).set_index('name')
+        res = pd.concat([res.reset_index(), cur_df.reset_index()], ignore_index=True).set_index('name')
+    
+        logger.debug('saving the results')
+        res.to_csv(fname_res)
+        df.to_csv(fname_df)
+
+    finally:
+        driver.quit()
+
+
+
+if __name__ == "__main__":
+
+    # n = 15
+    # year = 2015
+    # fname_df = 
+    # fname_res = 
+
+    n = 3
+    year = 2014
+
+    fname_df = os.path.join('data', 'universities_table.csv')
+    fname_res = os.path.join('data', 'patent_count.csv')
+
+    for i in range(1):
+        main(n=n, year=year, fname_df=fname_df, fname_res=fname_res)
+        time.sleep(5)
+
