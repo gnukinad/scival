f3fb2c7b83548894602cd5d409fc4735808ef979
DaniyarBakir <daniyar.bakir@nu.edu.kz>
2018-04-03 17:14:21 +0600

    added a new file which changes the order of the affilliations to search

---
+++ src/change_aff_order.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+BASE_DIR = os.path.abspath(os.path.realpath(__file__))
+BASE_DIR = os.path.join(os.path.dirname(BASE_DIR), '..')
+os.chdir(BASE_DIR)
+
+FOLNAME_AFF_SEARCH = os.path.join(BASE_DIR, 'data', 'aff_search')
+FOLNAME_METRIC_RESPONSE = os.path.join(BASE_DIR, 'data', 'metric_response')
+
+if __name__ == "__main__":
+
+    fname_df_order = os.path.join('data', 'universities_order.xlsx')
+    fname_df_aff   = os.path.join('data', 'universities_table.csv')
+
+    df_order = pd.read_excel(fname_df_order, 'Sheet5').set_index('name').replace(np.nan, '')
+    df_aff = pd.read_csv(fname_df_aff).set_index("Institution")
+
+    df_order = df_order.assign(order=range(df_order.shape[0]))
+
+    if 'order' not in df_aff.columns.tolist():
+        df_aff = df_aff.assign(order=0)
+
+    df_order_index = df_order.index.tolist()
+
+    maxv = np.max(df_order['order'].tolist())
+    i = 1
+
+    for index, row in df_aff.iterrows():
+
+        if index in df_order_index:
+
+            if df_order.at[index, 'order'] == '':
+
+                df_aff.at[index, 'order'] = maxv + i
+                i = i + 1
+            else:
+
+                df_aff.at[index, 'order'] = df_order.at[index, 'order']
+
+    df_aff.to_csv(fname_df_aff)
