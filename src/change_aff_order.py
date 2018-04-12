f31266b32a793da0b2234862f8bdd81296028df3
DaniyarBakir <daniyar.bakir@nu.edu.kz>
2018-04-12 12:38:22 +0600

    fixed an error in issuing a new order

--- src/change_aff_order.py
+++ src/change_aff_order.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     df_order = pd.read_excel(fname_df_order).set_index('name').replace(np.nan, '')
     df_aff = pd.read_csv(fname_df_aff).set_index("Institution")
 
-    df_order = df_order.assign(order=range(df_order.shape[0]))
+    if 'order' not in df_order.columns.tolist():
+        df_order = df_order.assign(order=range(df_order.shape[0]))
 
     if 'order' not in df_aff.columns.tolist():
         df_aff = df_aff.assign(order=0)
@@ -28,16 +29,27 @@ if __name__ == "__main__":
     maxv = np.max(df_order['order'].tolist())
     i = 1
 
+    # for index, row in df_aff.iloc[:200, :].iterrows():
     for index, row in df_aff.iterrows():
 
         if index in df_order_index:
 
+            print('before applying order ', df_order.at[index, 'order'])
+            print('index is ', index, '\tvalue is ', df_order.at[index, 'order'])
+
             if df_order.at[index, 'order'] == '':
+
+                print('epmty')
 
                 df_aff.at[index, 'order'] = maxv + i
                 i = i + 1
             else:
 
+                print('not empty')
                 df_aff.at[index, 'order'] = df_order.at[index, 'order']
+
+
+            print('after applying order ', df_order.at[index, 'order'])
+            print()
 
     df_aff.to_csv(fname_df_aff)
